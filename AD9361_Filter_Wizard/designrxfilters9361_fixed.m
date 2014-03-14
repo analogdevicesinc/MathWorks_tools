aee825ff4e306cf4075d5e4906eac2f1c7f780c6
dpu1778 <di.pu@analog.com>
2014-03-14 15:37:40 -0400

    rename "dBstopmin"
    change it to "dBstop_FIR"

--- AD9361_Filter_Wizard/designrxfilters9361_fixed.m
+++ AD9361_Filter_Wizard/designrxfilters9361_fixed.m
@@ -8,7 +8,7 @@
 % Fstop      = stopband frequency (in Hz)
 % dBripple   = max ripple allowed in passband (in dB)
 % dBstop     = min attenuation in stopband (in dB)
-% dBstopmin  = min rejection that TFIR is required to have (in dB)
+% dBstop_FIR = min rejection that TFIR is required to have (in dB)
 % phEQ       = Phase Equalization on (not -1)/off (-1)
 % int_FIR    = Use AD9361 FIR on (1)/off (0)
 % wnom       = analog cutoff frequency (in Hz)
@@ -18,6 +18,6 @@
 % rfirtaps         = fixed point coefficients for AD9361
 % rxFilters        = system object for visualization
 %
-function [rfirtaps,rxFilters] = designrxfilters9361_fixed(Fout,FIR_interp,HB_interp,PLL_mult,Fpass,Fstop,dBripple,dBstop,dBstopmin,phEQ,int_FIR,wnom)
+function [rfirtaps,rxFilters] = designrxfilters9361_fixed(Fout,FIR_interp,HB_interp,PLL_mult,Fpass,Fstop,dBripple,dBstop,dBstop_FIR,phEQ,int_FIR,wnom)
 
-[rfirtaps,rxFilters,~,~,~] = internal_designrxfilters9361_fixed(Fout,FIR_interp,HB_interp,PLL_mult,Fpass,Fstop,dBripple,dBstop,dBstopmin,phEQ,int_FIR,wnom);
+[rfirtaps,rxFilters,~,~,~] = internal_designrxfilters9361_fixed(Fout,FIR_interp,HB_interp,PLL_mult,Fpass,Fstop,dBripple,dBstop,dBstop_FIR,phEQ,int_FIR,wnom);
