486cb31310670649b91f777b45aa654eb41a7810
Tim Harder <timothy.harder@analog.com>
2014-12-19 17:15:18 -0500

    Filter Wizard: switch title for Decimation/Interpolation rates panel
    It's now set properly related to whether an Rx or Tx filter is being
    designed.
    
    Signed-off-by: Tim Harder <timothy.harder@analog.com>

--- AD9361_Filter_Wizard/AD9361_Filter_Wizard.m
+++ AD9361_Filter_Wizard/AD9361_Filter_Wizard.m
@@ -1102,14 +1102,17 @@ function data2gui(hObject, handles)
 
 OK = 1;
 
+rates_uipanel = findall(gcf, 'type', 'uipanel', 'Tag', 'uipanel5');
 if get(handles.filter_type, 'Value') == 1
     % Receive
     sel = handles.input_rx;
     max_HB = handles.MAX_RX;
+    set(rates_uipanel, 'Tag', 'uipanel5', 'Title', 'AD936x Decimation Rates');
 else
     % Transmit
     sel = handles.input_tx;
     max_HB = handles.MAX_TX;
+    set(rates_uipanel, 'Tag', 'uipanel5', 'Title', 'AD936x Interpolation Rates');
 end
 
 % set things from the file.
