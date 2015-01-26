502837c5176a62e7fc0e9754d8cd66fc5380a54c
dpu1778 <di.pu@analog.com>
2015-01-26 15:00:39 -0500

    HIL: Updated the QPSK example

--- hil_models/qpsktxrx/berr.m
+++
@@ -1,4 +0,0 @@
-function err = berr(u)
-    u_norm = complex(0.75 * abs(real(u)) / max(abs(real(u))), 0.75 * abs(imag(u)) / max(abs(imag(u))));
-    err = sum((real(u_norm) - 0.75).^2) + sum((imag(u_norm) - 0.75).^2);
-end
