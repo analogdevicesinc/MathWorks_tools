cee30a9bccef4ac3db62c78f7fbc8dd2b2fa1b68
Tim Harder <timothy.harder@analog.com>
2015-02-10 10:46:03 -0500

    Filter Wizard: calculate divider value properly in nonadvanced case
    Previously we were assuming the calibration divider value had already
    been calculated leading to cases where it could be 0 in nonadvanced
    cases which would lead to wrong values for the RF bandwidth.
    
    Signed-off-by: Tim Harder <timothy.harder@analog.com>

--- AD9361_Filter_Wizard/AD9361_Filter_Wizard.m
+++ AD9361_Filter_Wizard/AD9361_Filter_Wizard.m
@@ -892,7 +892,11 @@ converter_rate = get_converter_rate(handles);
 
 % determine the RF bandwidth from the current caldiv
 pll_rate = get_pll_rate(handles);
-caldiv = sel.caldiv;
+if get(handles.Advanced_options, 'Value') == 0
+    caldiv = default_caldiv(handles);
+else
+    caldiv = get_caldiv(handles);
+end
 RFbw = get_rfbw(handles, caldiv);
 RFbw_hw = get_rfbw_hw(handles, caldiv);
 
