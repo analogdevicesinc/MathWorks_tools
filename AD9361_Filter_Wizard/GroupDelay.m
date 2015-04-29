0f607d43ae926f794d263bc45bcbd48e32df002c
dpu1778 <di.pu@analog.com>
2015-04-29 14:44:27 -0400

    Filter Wizard: edit the function

--- AD9361_Filter_Wizard/GroupDelay.m
+++ AD9361_Filter_Wizard/GroupDelay.m
@@ -1,26 +1,20 @@
-% phase = unwrap(angle(response));
-% phase = phase.*(180/pi);
+function t = GroupDelay(freq,phase)
+% Calculates the Group Delay from frequency data (in Hz) and phase data (in degrees).
 
-function t = GroupDelay(f,phase)
-% GroupDelay Calculates the Group Delay from frequency data (in Hz)
-%     and phase data (in degrees).
-%        Christopher J. Struck - 3 August 2000
-elements = size(phase);
-k = elements(2);
+k = length(phase);
+
 % Unwrap phase data
 for n = 2:k
-    while phase(n) - phase(n - 1) <= -180
+    if phase(n) - phase(n - 1) <= -180
         phase(n) = phase(n) + 360;
-    end
-    while phase(n) - phase(n - 1) >= 180
-        phase(n)= phase(n) - 360;
+    else
+        phase(n) = phase(n) - 360;
     end
 end
+
 % Calculate Group Delay
 for n = 2:k-1
-    t(n) = (-1/720) * (((phase(n) - phase(n - 1)) / (f(n) - f(n - 1))) + ((phase(n + 1) - phase(n)) / (f(n + 1) - f(n))));
+    t(n) = (-1/720) * (((phase(n) - phase(n - 1)) / (freq(n) - freq(n - 1))) + ((phase(n + 1) - phase(n)) / (freq(n + 1) - freq(n))));
 end
-t(1) = (-1/360) * (((phase(2) - phase(1))/(f(2) - f(1))));
-t(k) = (-1/360) * (((phase(k) - phase(k - 1))/(f(k) - f(k - 1))));
-
-% plot(omega,t);
+t(1) = (-1/360) * (((phase(2) - phase(1))/(freq(2) - freq(1))));
+t(k) = (-1/360) * (((phase(k) - phase(k - 1))/(freq(k) - freq(k - 1))));
