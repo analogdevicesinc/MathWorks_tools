739d75f56ff9d54fa9dab17c655b5400d5c3aaf7
dpu1778 <di.pu@analog.com>
2015-07-22 13:59:39 -0400

    ADSB: added a function to convert degree
    The new function converts degrees in decimal format to DD:MM:SS format.
    That will make it possible to run the code without needing Mapping
    Toolbox.

---
+++ hil_models/ADSB_MATLAB/ConvertFracDeg.m
@@ -0,0 +1,29 @@
+function [dd,mm,ss] = ConvertFracDeg(degIn)
+
+if degIn>0
+    dd=floor(degIn);
+else
+    dd=ceil(degIn);
+end
+fracd = abs(degIn-dd);
+if dd==0
+    if degIn>0
+        mm=floor(fracd*60);
+        fracm = fracd-(mm/60);
+    else
+        mm=-floor(fracd*60);
+        fracm = fracd+(mm/60);
+    end
+else
+    mm=floor(fracd*60);
+    fracm = fracd-(mm/60);
+end
+if (dd==0 && mm==0)
+    if degIn>0
+        ss=(fracm*3600);
+    else
+        ss=-(fracm*3600);
+    end
+else
+    ss=(fracm*3600);
+end
