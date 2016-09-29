7ece3547d9e2ae46d5f761d5d5f6551bc4cf8c96
acozma <andrei.cozma@analog.com>
2016-09-29 15:27:33 +0300

    Fixed the code targeting specs

--- hdl_wa_bsp/vendor/AnalogDevices/+AnalogDevices/install.m
+++ hdl_wa_bsp/vendor/AnalogDevices/+AnalogDevices/install.m
@@ -26,13 +26,29 @@ function install(mode)
     hdlbsp.util.vendorInstall(mode,paths);
 	
 	% Copy the Zynq SDR target definition file into the support package
+	source  = [];
+	destination = [];
 	zynqRootDir   = codertarget.zynq.internal.getSpPkgRootDir;
+	
 	zynqTargetDir = fullfile(zynqRootDir,'registry/targethardware');
-	source = fullfile(vendorRootDir, '/+AnalogDevices/+util/adizynqsdr.xml');
-	destination = fullfile(zynqTargetDir, 'adizynqsdr.xml');
+	source = [source {fullfile(vendorRootDir, '/+AnalogDevices/+util/adizynqsdr.xml')}];
+	destination = [destination {fullfile(zynqTargetDir, 'adizynqsdr.xml')}];
+	
+	zynqTargetDir = fullfile(zynqRootDir,'registry/attributes');
+	source = [source {fullfile(vendorRootDir, '/+AnalogDevices/+util/ADIZynqSDRAttributeInfo.xml')}];
+	destination = [destination {fullfile(zynqTargetDir, 'ADIZynqSDRAttributeInfo.xml')}];
+	
+	zynqTargetDir = fullfile(zynqRootDir,'registry/parameters');
+	source = [source {fullfile(vendorRootDir, '/+AnalogDevices/+util/ADIZynqSDRParameterInfo.xml')}];
+	destination = [destination {fullfile(zynqTargetDir, 'ADIZynqSDRParameterInfo.xml')}];
+	
 	if(mode == 0)
-		copyfile(source, destination);
-	else
-		delete(destination);
+        for i = 1:length(source)
+            copyfile(char(source(:,i)), char(destination(:,i)), 'f');
+        end
+    else
+        for i = 1:length(destination)
+            delete(char(destination(:,i)));
+        end
 	end	
 end
