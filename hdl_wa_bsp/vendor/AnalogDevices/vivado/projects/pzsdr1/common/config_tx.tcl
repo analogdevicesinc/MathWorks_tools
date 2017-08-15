062f58c05c30e2f3a0a4dcd23fdce758d09b2d94
acozma <andrei.cozma@analog.com>
2017-08-15 17:59:14 +0300

    RFSOM Tx fixes

--- hdl_wa_bsp/vendor/AnalogDevices/vivado/projects/pzsdr1/common/config_tx.tcl
+++ hdl_wa_bsp/vendor/AnalogDevices/vivado/projects/pzsdr1/common/config_tx.tcl
@@ -1,8 +1,8 @@
 # Disconnect the DAC UNPACK pins
-delete_bd_objs [get_bd_nets axi_ad9361_dac_valid_i0]
-delete_bd_objs [get_bd_nets axi_ad9361_dac_valid_q0]
-delete_bd_objs [get_bd_nets axi_ad9361_dac_valid_i1]
-delete_bd_objs [get_bd_nets axi_ad9361_dac_valid_q1]
+delete_bd_objs [get_bd_nets dac_fifo_din_valid_0]
+delete_bd_objs [get_bd_nets dac_fifo_din_valid_1]
+delete_bd_objs [get_bd_nets dac_fifo_din_valid_2]
+delete_bd_objs [get_bd_nets dac_fifo_din_valid_3]
 
 delete_bd_objs [get_bd_nets util_ad9361_dac_upack_dac_data_0]
 delete_bd_objs [get_bd_nets util_ad9361_dac_upack_dac_data_1]
@@ -10,7 +10,6 @@ delete_bd_objs [get_bd_nets util_ad9361_dac_upack_dac_data_2]
 delete_bd_objs [get_bd_nets util_ad9361_dac_upack_dac_data_3]
 
 # Connect the DAC UNPACK valid signals together
-connect_bd_net [get_bd_pins util_ad9361_dac_upack/unpack_valid_0] [get_bd_pins util_ad9361_dac_upack/unpack_valid_1]
-connect_bd_net [get_bd_pins util_ad9361_dac_upack/unpack_valid_0] [get_bd_pins util_ad9361_dac_upack/unpack_valid_2]
-connect_bd_net [get_bd_pins util_ad9361_dac_upack/unpack_valid_0] [get_bd_pins util_ad9361_dac_upack/unpack_valid_3]
-
+connect_bd_net [get_bd_pins util_ad9361_dac_upack/dac_valid_0] [get_bd_pins util_ad9361_dac_upack/dac_valid_1]
+connect_bd_net [get_bd_pins util_ad9361_dac_upack/dac_valid_0] [get_bd_pins util_ad9361_dac_upack/dac_valid_2]
+connect_bd_net [get_bd_pins util_ad9361_dac_upack/dac_valid_0] [get_bd_pins util_ad9361_dac_upack/dac_valid_3]
