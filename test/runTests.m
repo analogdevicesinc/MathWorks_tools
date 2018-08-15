add74dded891034c6eb8f26faeda3519700408ad
Travis Collins <travis.collins@analog.com>
2018-08-15 18:37:56 -0400

    Filter out specialized builds from standard tests.  Also print post failure data at test conclusions.
    Signed-off-by: Travis Collins <travis.collins@analog.com>

--- test/runTests.m
+++ test/runTests.m
@@ -1,4 +1,10 @@
 results = run(BSPTests);
 t = table(results);
 disp(t);
+disp('################################################################################');
+for test = results
+    if test.Failed
+        disp(results.Details.DiagnosticRecord.Report);
+    end
+end
 save(['BSPTest_',datestr(now,'dd_mm_yyyy-HH:MM:SS'),'.mat'],'t');
