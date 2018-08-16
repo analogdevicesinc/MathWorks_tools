961976fce99332587c4bf4d0056eaaeefc963b1e
Travis Collins <travis.collins@analog.com>
2018-08-16 19:10:33 -0400

    Update bsp projectfile naming

--- CI/scripts/genTlbx.m
+++ CI/scripts/genTlbx.m
@@ -1,6 +1,6 @@
 cd(fileparts((mfilename('fullpath'))));
 cd ../..
-projectFile = 'CI/scripts/BSP.prj';
+projectFile = 'CI/scripts/bsp.prj';
 currentVersion = matlab.addons.toolbox.toolboxVersion(projectFile);
 outputFile = ['AnalogDevicesBSP_v',currentVersion];
 matlab.addons.toolbox.packageToolbox(projectFile,outputFile)
