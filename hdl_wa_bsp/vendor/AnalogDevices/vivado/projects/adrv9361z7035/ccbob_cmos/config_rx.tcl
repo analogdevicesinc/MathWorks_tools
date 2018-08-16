784c2ca5cdf28495410919ae58616cbaf8ae93d4
Travis Collins <travis.collins@analog.com>
2018-08-16 11:29:54 -0400

    Remove vivado folder since it is built automagically now.
    Signed-off-by: Travis Collins <travis.collins@analog.com>

--- hdl_wa_bsp/vendor/AnalogDevices/vivado/projects/adrv9361z7035/ccbob_cmos/config_rx.tcl
+++
@@ -1,7 +0,0 @@
-set ad_hdl_dir    	[pwd]
-set proj_dir		$ad_hdl_dir/projects/adrv9361z7035/ccbob_cmos
-
-source $proj_dir/config_prj.tcl
-source $ad_hdl_dir/projects/adrv9361z7035/common/config_rx.tcl
-
-regenerate_bd_layout
--- hdl_wa_bsp/vendor/AnalogDevices/vivado/projects/fmcomms5/zc706/config_tx.tcl
+++
@@ -1,7 +0,0 @@
-set ad_hdl_dir    	[pwd]
-set proj_dir		$ad_hdl_dir/projects/fmcomms5/zc706
-
-source $proj_dir/config_prj.tcl
-source $ad_hdl_dir/projects/fmcomms5/common/config_tx.tcl
-
-regenerate_bd_layout
