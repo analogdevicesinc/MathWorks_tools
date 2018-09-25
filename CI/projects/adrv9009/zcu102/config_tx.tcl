d7971d002e181d937631f1b2ecffed454fc2f27f
Travis Collins <travis.collins@analog.com>
2018-09-24 21:40:29 -0400

    Add tx support for ADRV9009
    Signed-off-by: Travis Collins <travis.collins@analog.com>

---
+++ CI/projects/adrv9009/zcu102/config_tx.tcl
@@ -0,0 +1,7 @@
+set ad_hdl_dir    	[pwd]
+set proj_dir		$ad_hdl_dir/projects/adrv9009/zcu102
+
+source $proj_dir/config_prj.tcl
+source $ad_hdl_dir/projects/adrv9009/common/config_tx.tcl
+
+regenerate_bd_layout
---
+++ hdl_wa_bsp/vendor/AnalogDevices/+AnalogDevices/+adrv9009/+zcu102/+tx/plugin_board.m
@@ -0,0 +1,9 @@
+function hP = plugin_board()
+% Zynq Platform PCore
+% Use Plugin API to create board plugin object
+
+%   Copyright 2015 The MathWorks, Inc.
+
+% Call the common board definition function
+hP = AnalogDevices.adrv9009.common.plugin_board('ZCU102', 'Tx');
+
