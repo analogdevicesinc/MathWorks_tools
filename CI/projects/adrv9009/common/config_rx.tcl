9e3f4fc22c64fefa84abd64c265154ac9162de58
Travis Collins <travis.collins@analog.com>
2018-09-24 21:37:28 -0400

    Fix clock naming for AXI interface for ADRV9009
    Signed-off-by: Travis Collins <travis.collins@analog.com>

--- CI/projects/adrv9009/common/config_rx.tcl
+++ CI/projects/adrv9009/common/config_rx.tcl
@@ -14,3 +14,6 @@ disconnect_bd_net /axi_adrv9009_core_adc_data_q1 [get_bd_pins util_adrv9009_rx_c
 connect_bd_net [get_bd_pins util_adrv9009_rx_cpack/adc_valid_0] [get_bd_pins util_adrv9009_rx_cpack/adc_valid_1]
 connect_bd_net [get_bd_pins util_adrv9009_rx_cpack/adc_valid_0] [get_bd_pins util_adrv9009_rx_cpack/adc_valid_2]
 connect_bd_net [get_bd_pins util_adrv9009_rx_cpack/adc_valid_0] [get_bd_pins util_adrv9009_rx_cpack/adc_valid_3]
+
+# Connect clock
+connect_bd_net -net [get_bd_nets axi_adrv9009_rx_clkgen] [get_bd_pins axi_cpu_interconnect/M13_ACLK] [get_bd_pins axi_adrv9009_rx_clkgen/clk_0]
