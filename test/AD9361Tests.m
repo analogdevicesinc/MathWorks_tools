726c04b71022c06ef0ea5cd33a4d923357eed531
Travis Collins <travis.collins@analog.com>
2018-10-22 20:45:42 -0400

    Add more integration with libad9361
    Signed-off-by: Travis Collins <travis.collins@analog.com>

--- test/AD9361Tests.m
+++ test/AD9361Tests.m
@@ -1,10 +1,14 @@
 classdef AD9361Tests < matlab.unittest.TestCase
     
+    properties
+        uri = 'ip:192.168.2.1';
+    end
+    
     methods (Test)
         
         function testAD9361Rx(testCase)    
             % Test Rx DMA data output
-            rx = adi.AD9361.Rx('uri','ip:192.168.3.2');
+            rx = adi.AD9361.Rx('uri',testCase.uri);
             rx.channelCount = 2;
             [out, valid] = rx();
             rx.release();
@@ -14,13 +18,13 @@ classdef AD9361Tests < matlab.unittest.TestCase
         
         function testAD9361RxWithTxDDS(testCase)
             % Test DDS output
-            tx = adi.AD9361.Tx('uri','ip:192.168.3.2');
+            tx = adi.AD9361.Tx('uri',testCase.uri);
             tx.DataSource = 'DDS';
             tx.DDSFrequencies = [5,5,5,5].*1e3;
             tx.Attenuation = -50;
             tx();
             pause(1);
-            rx = adi.AD9361.Rx('uri','ip:192.168.3.2');
+            rx = adi.AD9361.Rx('uri',testCase.uri);
             rx.channelCount = 2;
             valid = false;
             for k=1:10
@@ -41,12 +45,12 @@ classdef AD9361Tests < matlab.unittest.TestCase
             swv1.SampleRate = 3e6;
             y = swv1();
             
-            tx = adi.AD9361.Tx('uri','ip:192.168.3.2');
+            tx = adi.AD9361.Tx('uri',testCase.uri);
             tx.DataSource = 'DMA';
             tx.SamplesPerFrame = length(y);
             tx.EnableCyclicBuffers = true;
             tx(y);
-            rx = adi.AD9361.Rx('uri','ip:192.168.3.2');
+            rx = adi.AD9361.Rx('uri',testCase.uri);
             rx.channelCount = 2;
             for k=1:10
                 [out, valid] = rx();
