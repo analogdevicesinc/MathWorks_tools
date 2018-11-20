132d52ed1bfe364f94606ba12741b7fe050df7c1
Travis Collins <travis.collins@analog.com>
2018-11-20 17:29:39 -0500

    Added some board variants for ADRV with documentation
    Signed-off-by: Travis Collins <travis.collins@analog.com>

--- +adi/Contents.m
+++ +adi/Contents.m
@@ -5,20 +5,20 @@
 %
 % Parts
 % -----------------------
-%   <a href="matlab:help adi.ad9144            ">AD9144</a>         - High speed DAC
-%   <a href="matlab:help adi.ad9361            ">AD9361</a>         - Transceiver
-%   <a href="matlab:help adi.ad9363            ">AD9363</a>         - Transceiver
-%   <a href="matlab:help adi.ad9364            ">AD9364</a>         - Transceiver
-%   <a href="matlab:help adi.ad9371            ">AD9371</a>         - Transceiver
-%   <a href="matlab:help adi.ad9680            ">AD9680</a>         - High speed ADC
-%   <a href="matlab:help adi.adrv9009          ">ADRV9009</a>       - Transceiver
+%   <a href="matlab:help adi.AD9144            ">AD9144</a>         - High speed DAC
+%   <a href="matlab:help adi.AD9361            ">AD9361</a>         - Transceiver
+%   <a href="matlab:help adi.AD9363            ">AD9363</a>         - Transceiver
+%   <a href="matlab:help adi.AD9364            ">AD9364</a>         - Transceiver
+%   <a href="matlab:help adi.AD9371            ">AD9371</a>         - Transceiver
+%   <a href="matlab:help adi.AD9680            ">AD9680</a>         - High speed ADC
+%   <a href="matlab:help adi.ADRV9009          ">ADRV9009</a>       - Transceiver
 %
 % Boards and Platforms
 % -----------------------
 %   <a href="matlab:help adi.DAQ2              ">DAQ2</a>           - FMC development board for high speed data acquisition
-%   <a href="matlab:help adi.FMComms2          ">FMComms2/3</a>     - FMC development board based on the AD9361
+%   <a href="matlab:help adi.FMComms2          ">FMComms2</a>       - FMC development board based on the AD9361 with narrowband baluns
+%   <a href="matlab:help adi.FMComms3          ">FMComms3</a>       - FMC development board based on the AD9361 with wideband baluns
 %   <a href="matlab:help adi.FMComms4          ">FMComms4</a>       - FMC development board based on the AD9364
-%   <a href="matlab:help adi.FMComms5          ">FMComms5</a>       - FMC development board based on dual AD9361s
 %   <a href="matlab:help adi.PackRF            ">PackRF</a>         - Portable Reference design based on the AD9361 with Zynq-Z7035
 %   <a href="matlab:help adi.Pluto             ">Pluto</a>          - Portable self-contained RF learning module based on the AD9363 with Zynq-Z7020
 %   <a href="matlab:help adi.ADRV9361Z7035     ">ADRV9361-Z7035</a> - SOM based on the AD9361 with Zynq-Z7035
