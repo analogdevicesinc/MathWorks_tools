a85650dde942666a283bbbff4b038c4eb0e91d79
Travis Collins <travis.collins@analog.com>
2019-01-10 11:49:52 -0500

    Remove hdl_prj folder for ADSB demo which can be generated from the models themselves. This saves more than 100MB in the final toolbox installer.
    Signed-off-by: Travis Collins <travis.collins@analog.com>

--- targeting_models/ADSB/hdl_prj/vivado_ip_prj/projects/pzsdr2/ccbrk_lvds/config_rx_tx.tcl
+++
@@ -1,8 +0,0 @@
-set ad_hdl_dir    	[pwd]
-set proj_dir		$ad_hdl_dir/projects/pzsdr2/ccbrk_lvds
-
-source $proj_dir/config_prj.tcl
-source $ad_hdl_dir/projects/pzsdr2/common/config_rx.tcl
-source $ad_hdl_dir/projects/pzsdr2/common/config_tx.tcl
-
-regenerate_bd_layout
