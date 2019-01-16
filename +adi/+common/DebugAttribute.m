878f552c020f2f731ac38bf2133ab0bbbd5687c6
SrikanthPagadarai <srikanth.pagadarai@analog.com>
2019-01-16 17:19:25 -0500

    Added functions to set debug attributes and to read/write hardware registers directly.

---
+++ +adi/+common/DebugAttribute.m
@@ -0,0 +1,36 @@
+classdef (Abstract) DebugAttribute < matlabshared.libiio.base 
+    
+    methods (Hidden, Access = protected)
+        function setDebugAttributeLongLong(obj,attr,value)
+            phydev = getDev(obj, obj.phyDevName);
+            status = iio_device_debug_attr_write_longlong(obj,phydev,attr,value);
+            cstatus(obj,status,['Attribute write failed for : ' attr ' with value ' num2str(value)]);
+            % Check
+            [status, rValue] = iio_device_debug_attr_read_longlong(obj,phydev,attr);
+            cstatus(obj,status,['Error reading attribute: ' attr]);
+            if nargin<6
+                tol = sqrt(eps);
+            end
+            if abs(value - rValue) > tol
+                status = -1;
+                cstatus(obj,status,['Attribute ' attr ' return value ' num2str(rValue) ', expected ' num2str(value)]);
+            end
+            status = iio_device_debug_attr_write_longlong(obj,phydev, 'initialize',1);
+        end
+        
+        function setDebugAttributeBool(obj,attr,value)
+            phydev = getDev(obj, obj.phyDevName);
+            status = iio_device_debug_attr_write_bool(obj,phydev,attr,value);
+            cstatus(obj,status,['Attribute write failed for : ' attr]);
+            % Check
+            [status, rValue] = iio_device_debug_attr_read_bool(obj,phydev,attr);
+            cstatus(obj,status,['Error reading attribute: ' attr]);
+            if value ~= rValue
+                status = -1;
+                cstatus(obj,status,['Attribute ' attr ' return value ' num2str(rValue) ', expected ' num2str(value)]);
+            end
+            status = iio_device_debug_attr_write_bool(obj,phydev, 'initialize',1);
+        end        
+        
+    end    
+end
