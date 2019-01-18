ae19ad49b065291784b1eaf7135aecaf4fcb1ad1
SrikanthPagadarai <srikanth.pagadarai@analog.com>
2019-01-17 22:05:54 -0500

    Removed unnecessary nargin check and device initialization function call.

--- +adi/+common/DebugAttribute.m
+++ +adi/+common/DebugAttribute.m
@@ -3,23 +3,27 @@ classdef (Abstract) DebugAttribute < matlabshared.libiio.base
     methods (Hidden, Access = protected)
         function setDebugAttributeLongLong(obj,attr,value)
             phydev = getDev(obj, obj.phyDevName);
+            if (nargin == 1)
+                iio_device_debug_attr_write_longlong(obj,phydev, 'initialize',1);
+                return;
+            end
             status = iio_device_debug_attr_write_longlong(obj,phydev,attr,value);
             cstatus(obj,status,['Attribute write failed for : ' attr ' with value ' num2str(value)]);
             % Check
             [status, rValue] = iio_device_debug_attr_read_longlong(obj,phydev,attr);
             cstatus(obj,status,['Error reading attribute: ' attr]);
-            if nargin<6
-                tol = sqrt(eps);
-            end
-            if abs(value - rValue) > tol
+            if (value ~= rValue)
                 status = -1;
                 cstatus(obj,status,['Attribute ' attr ' return value ' num2str(rValue) ', expected ' num2str(value)]);
-            end
-            status = iio_device_debug_attr_write_longlong(obj,phydev, 'initialize',1);
+            end            
         end
         
         function setDebugAttributeBool(obj,attr,value)
             phydev = getDev(obj, obj.phyDevName);
+            if (nargin == 1)
+                iio_device_debug_attr_write_bool(obj,phydev, 'initialize',1);
+                return;
+            end
             status = iio_device_debug_attr_write_bool(obj,phydev,attr,value);
             cstatus(obj,status,['Attribute write failed for : ' attr]);
             % Check
@@ -28,9 +32,7 @@ classdef (Abstract) DebugAttribute < matlabshared.libiio.base
             if value ~= rValue
                 status = -1;
                 cstatus(obj,status,['Attribute ' attr ' return value ' num2str(rValue) ', expected ' num2str(value)]);
-            end
-            status = iio_device_debug_attr_write_bool(obj,phydev, 'initialize',1);
-        end        
-        
-    end    
+            end            
+        end                
+    end       
 end
