361d0dd89d5ccaa8ff5cb94dff42121d350d040b
Travis Collins <travis.collins@analog.com>
2019-01-24 11:28:28 -0500

    Add ADRV9371+ZC706 targeting support
    Signed-off-by: Travis Collins <travis.collins@analog.com>

--- CI/projects/common/zc706/zc706_plddr3_dacfifo_bd.tcl
+++ CI/projects/common/zc706/zc706_plddr3_dacfifo_bd.tcl
@@ -4,9 +4,9 @@
 ad_ip_instance proc_sys_reset axi_rstgen
 ad_ip_instance mig_7series axi_ddr_cntrl
 
-file copy -force $ad_hdl_dir/projects/common/zc706/zc706_plddr3_mig.prj [get_property IP_DIR \
+file copy -force $ad_hdl_dir/projects/common/zc706/zc706_plddr3_mig.mk [get_property IP_DIR \
   [get_ips [get_property CONFIG.Component_Name [get_bd_cells axi_ddr_cntrl]]]]
-ad_ip_parameter axi_ddr_cntrl CONFIG.XML_INPUT_FILE zc706_plddr3_mig.prj
+ad_ip_parameter axi_ddr_cntrl CONFIG.XML_INPUT_FILE zc706_plddr3_mig.mk
 
 create_bd_port -dir I -type rst sys_rst
 set_property CONFIG.POLARITY ACTIVE_HIGH [get_bd_ports sys_rst]
