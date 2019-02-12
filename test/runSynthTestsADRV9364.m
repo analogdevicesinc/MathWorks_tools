65126ac90a35445c8520a71df36fed988ff8c7b7
Travis Collins <travis.collins@analog.com>
2019-02-11 20:03:09 -0800

    Split synthesis into separate jobs based on board type
    Signed-off-by: Travis Collins <travis.collins@analog.com>

---
+++ test/runSynthTestsADRV9364.m
@@ -0,0 +1,37 @@
+import matlab.unittest.TestRunner;
+import matlab.unittest.TestSuite;
+import matlab.unittest.plugins.TestReportPlugin;
+import matlab.unittest.plugins.XMLPlugin
+import matlab.unittest.parameters.Parameter
+
+SynthesizeDesign = {true};
+
+param = Parameter.fromData('SynthesizeDesign',SynthesizeDesign);
+
+try
+    suite = TestSuite.fromClass(?BSPTests,'ExternalParameters',param);
+    suite = suite.selectIf('ParameterProperty','configs', 'ParameterName','*adrv9364*');
+
+    runner = TestRunner.withNoPlugins;
+    xmlFile = 'BSPSynthTestResults.xml';
+    plugin = XMLPlugin.producingJUnitFormat(xmlFile);
+    
+    runner.addPlugin(plugin);
+    results = runner.run(suite);
+    
+    t = table(results);
+    disp(t);
+    disp(repmat('#',1,80));
+    for test = results
+        if test.Failed
+            disp(test.Name);
+        end
+    end
+catch e
+    disp(getReport(e,'extended'));
+    bdclose('all');
+    exit(1);
+end
+save(['BSPTest_',datestr(now,'dd_mm_yyyy-HH:MM:SS'),'.mat'],'t');
+bdclose('all');
+exit(any([results.Failed]));
