d23b53432e84156ed69a3e0b2444ba258b8fb21e
Travis Collins <travis.collins@analog.com>
2019-02-28 20:15:57 -0500

    Add initial support for ADRV9009 custom profiles
    Signed-off-by: Travis Collins <travis.collins@analog.com>

--- +adi/+ADRV9009/Base.m
+++ +adi/+ADRV9009/Base.m
@@ -15,6 +15,19 @@ classdef (Abstract, Hidden = true) Base < adi.common.Attribute & matlabshared.li
         channelCount = 2;
     end
     
+    properties (Nontunable, Logical)
+        %EnableCustomProfile Enable Custom Profile
+        %   Enable use of custom Profile file to set SamplingRate, 
+        %   RFBandwidth, and FIR in datapaths
+        EnableCustomProfile = false;
+    end
+    
+    properties (Nontunable)
+        %CustomProfileFileName Custom Profile File Name
+        %   Path to custom Profile file created from profile wizard
+        CustomProfileFileName = '';
+    end
+    
     properties (Hidden, Constant)
         %SamplingRate Sampling Rate
         %   Baseband sampling rate in Hz, specified as a scalar 
@@ -80,7 +93,12 @@ classdef (Abstract, Hidden = true) Base < adi.common.Attribute & matlabshared.li
         function icon = getIconImpl(obj)
             icon = sprintf(['ADRV9009 ',obj.Type]);
         end
-                   
+           
+        function writeProfileFile(obj)
+            profle_data_str = fileread(obj.CustomProfileFileName);
+            obj.setDeviceAttributeRAW('profile_config',profle_data_str);
+        end
+        
     end
     
     %% External Dependency Methods
