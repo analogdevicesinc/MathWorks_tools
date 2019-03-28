a7ae4ad8d3ba4b6d3209136cef895a5a134e12bd
Travis Collins <travis.collins@analog.com>
2019-03-27 20:30:00 -0400

    Simply FMComms2/3 targeting menu and tcl scripts
    Signed-off-by: Travis Collins <travis.collins@analog.com>

--- CI/projects/fmcomms2/zed/config_tx.tcl
+++
@@ -1,7 +0,0 @@
-set ad_hdl_dir    	[pwd]
-set proj_dir		$ad_hdl_dir/projects/fmcomms2/zed
-
-source $proj_dir/config_prj.tcl
-source $ad_hdl_dir/projects/fmcomms2/common/config_tx.tcl
-
-regenerate_bd_layout
---
+++ hdl_wa_bsp/vendor/AnalogDevices/+AnalogDevices/+fmcomms2/+zc702/plugin_rd_rxtx.m
@@ -0,0 +1,8 @@
+function hRD = plugin_rd_rxtx
+% Reference design definition
+
+%   Copyright 2014-2015 The MathWorks, Inc.
+
+% Call the common reference design definition function
+hRD = AnalogDevices.fmcomms2.common.plugin_rd('ZC702', 'Rx & Tx');
+AnalogDevices.fmcomms2.zc702.add_io(hRD,'Rx & Tx');
