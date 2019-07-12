404d20e1fcd7cd45846940e4e540fede40831cf6
Travis F. Collins <travis.collins@analog.com>
2019-07-12 11:26:58 -0700

    Fix TX DMA valid mapping in AD936X HDL projects. Fixes #15
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- CI/projects/adrv9361z7035/common/config_tx.tcl
+++ CI/projects/adrv9361z7035/common/config_tx.tcl
@@ -1,15 +1,16 @@
 # Disconnect the DAC UNPACK pins
-delete_bd_objs [get_bd_nets axi_ad9361_dac_fifo_din_valid_0]
-delete_bd_objs [get_bd_nets axi_ad9361_dac_fifo_din_valid_1]
-delete_bd_objs [get_bd_nets axi_ad9361_dac_fifo_din_valid_2]
-delete_bd_objs [get_bd_nets axi_ad9361_dac_fifo_din_valid_3]
+delete_bd_objs [get_bd_nets util_ad9361_dac_upack_dac_valid_out_0]
+delete_bd_objs [get_bd_nets util_ad9361_dac_upack_dac_valid_out_1]
+delete_bd_objs [get_bd_nets util_ad9361_dac_upack_dac_valid_out_2]
+delete_bd_objs [get_bd_nets util_ad9361_dac_upack_dac_valid_out_3]
 
+# Connect fifo valids together
+connect_bd_net [get_bd_pins axi_ad9361_dac_fifo/din_valid_in_1] [get_bd_pins axi_ad9361_dac_fifo/din_valid_in_0]
+connect_bd_net [get_bd_pins axi_ad9361_dac_fifo/din_valid_in_2] [get_bd_pins axi_ad9361_dac_fifo/din_valid_in_0]
+connect_bd_net [get_bd_pins axi_ad9361_dac_fifo/din_valid_in_3] [get_bd_pins axi_ad9361_dac_fifo/din_valid_in_0]
+
+# Remove data lines where IP will go
 delete_bd_objs [get_bd_nets util_ad9361_dac_upack_dac_data_0]
 delete_bd_objs [get_bd_nets util_ad9361_dac_upack_dac_data_1]
 delete_bd_objs [get_bd_nets util_ad9361_dac_upack_dac_data_2]
 delete_bd_objs [get_bd_nets util_ad9361_dac_upack_dac_data_3]
-
-# Connect the DAC UNPACK valid signals together
-connect_bd_net [get_bd_pins util_ad9361_dac_upack/dac_valid_0] [get_bd_pins util_ad9361_dac_upack/dac_valid_1]
-connect_bd_net [get_bd_pins util_ad9361_dac_upack/dac_valid_0] [get_bd_pins util_ad9361_dac_upack/dac_valid_2]
-connect_bd_net [get_bd_pins util_ad9361_dac_upack/dac_valid_0] [get_bd_pins util_ad9361_dac_upack/dac_valid_3]
