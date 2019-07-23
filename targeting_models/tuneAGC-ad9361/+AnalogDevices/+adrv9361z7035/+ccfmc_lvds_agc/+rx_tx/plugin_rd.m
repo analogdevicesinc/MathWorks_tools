c18e6dde5fc2d95c763588e1592d0d988b204130
SrikanthPagadarai <srikanth.pagadarai@analog.com>
2019-07-23 18:31:15 -0400

    Removed unnecessary reference design files.

--- targeting_models/tuneAGC-ad9361/+AnalogDevices/+adrv9361z7035/+ccfmc_lvds_agc/+rx_tx/plugin_rd.m
+++
@@ -1,8 +0,0 @@
-function hRD = plugin_rd
-% Reference design definition
-
-%   Copyright 2014-2015 The MathWorks, Inc.
-
-% Call the common reference design definition function
-hRD = AnalogDevices.adrv9361z7035.common.plugin_rd('fmc lvds agc', 'Rx & Tx');
-AnalogDevices.adrv9361z7035.ccfmc_lvds_agc.rx_tx.add_rx_tx_io(hRD);
